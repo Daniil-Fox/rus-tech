2acca13a169e2f93054b3925d48b019d8265d285
Artyushenko Daniil <foxones80@gmail.com>
2024-10-25 23:19:10 +0600

    fixed some

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,69 +1,64 @@
 import {Swiper} from 'swiper'
+import { FreeMode } from 'swiper/modules';
 
-// window.addEventListener('DOMContentLoaded', () => {
+Swiper.use([FreeMode])
+window.addEventListener('DOMContentLoaded', () => {
 
-//   const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
-//     let swiper;
+  const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
+    let swiper;
 
-//     breakpoint = window.matchMedia(breakpoint);
+    breakpoint = window.matchMedia(breakpoint);
 
-//     const enableSwiper = function(className, settings) {
-//       swiper = new Swiper(className, settings);
+    const enableSwiper = function(className, settings) {
+      swiper = new Swiper(className, settings);
 
-//       if (callback) {
-//         callback(swiper);
-//       }
-//     }
+      if (callback) {
+        callback(swiper);
+      }
+    }
 
-//     const checker = function() {
-//       if (breakpoint.matches) {
-//         return enableSwiper(swiperClass, swiperSettings);
-//       } else {
-//         if (swiper !== undefined) swiper.destroy(true, true);
-//         return;
-//       }
-//     };
+    const checker = function() {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
+      }
+    };
 
-//     breakpoint.addEventListener('change', checker);
-//     checker();
-//   }
+    breakpoint.addEventListener('change', checker);
+    checker();
+  }
 
-//   const someFunc = (instance) => {
-//     if (instance) {
-//       instance.on('slideChange', function (e) {
-//         console.log('*** mySwiper.activeIndex', instance.activeIndex);
-//       });
-//     }
-//   };
+  // const someFunc = (instance) => {
+  //   if (instance) {
+  //     instance.on('slideChange', function (e) {
+  //       console.log('*** mySwiper.activeIndex', instance.activeIndex);
+  //     });
+  //   }
+  // };
 
-//   resizableSwiper(
-//     '(max-width: 1280px)',
-//     '.slider-1',
-//     {
-//       loop: true,
-//       spaceBetween: 32,
-//       slidesPerView: 1,
-//       pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//       },
-//     },
-//     someFunc
-//   );
+  resizableSwiper(
+    '(max-width: 900px)',
+    '.benefits__slider',
+    {
+      freemode: true,
+      spaceBetween: 20,
+      slidesPerView: 'auto',
 
-//   resizableSwiper(
-//     '(max-width: 1500px)',
-//     '.slider-2',
-//     {
-//       loop: true,
-//       spaceBetween: 10,
-//       slidesPerView: 3,
-//       freeMode: true,
-//       breakpoints: {
-//         1200: {
-//           spaceBetween: 20,
-//         }
-//       }
-//     }
-//   );
-// });
+    },
+    // someFunc
+  );
+  resizableSwiper(
+    '(max-width: 576px)',
+    '.prod__slider',
+    {
+      freemode: true,
+      spaceBetween: 20,
+      slidesPerView: 'auto',
+    },
+    // someFunc
+  );
+
+
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 import './_components.js';
+
+const menu = document.querySelector('.menu')
+
+if(menu){
+  const burger = document.querySelector('.header__burger')
+  burger.addEventListener('click', e => {
+    menu.classList.toggle('active')
+    burger.classList.toggle('active')
+  })
+}
