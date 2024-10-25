88f8b42e930b580567c3dedf6446720d8e6f39b4
Artyushenko Daniil <foxones80@gmail.com>
2024-10-26 01:14:57 +0600

    added new and response

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,2 +1,3 @@
 import './components/slider.js'
 import './components/faq.js'
+import './components/validate.js'
--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,28 @@ if(menu){
     burger.classList.toggle('active')
   })
 }
+
+
+const modalButtons = document.querySelectorAll('.modal-btn')
+const modal = document.querySelector('.modal')
+if(modal && modalButtons.length > 0){
+  const modalBody = modal.querySelector('.modal__body')
+  modalBody.addEventListener('click', e => e.stopPropagation())
+  const themeInput = modal.querySelector('input[type=hidden]')
+  modalButtons.forEach(el => {
+    if(el.dataset.theme){
+      themeInput.value = el.dataset.theme
+    }
+    el.addEventListener('click', e => {
+      e.preventDefault()
+
+      modal.classList.add('active')
+    })
+  })
+
+  modal.addEventListener('click', e => {
+    e.preventDefault()
+    modal.classList.remove('active')
+    themeInput.value = 'Консультация'
+  })
+}
